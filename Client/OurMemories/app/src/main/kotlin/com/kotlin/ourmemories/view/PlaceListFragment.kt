58ee7ba2b409107a653e6d5a8ebffd6475f22ed0
DESKTOP-HVM23RB\hee <lucya2@naver.com>
2017-11-07 20:24:05 +0900

    커밋테스트

--- Client/OurMemories/app/src/main/kotlin/com/kotlin/ourmemories/view/PlaceListFragment.kt
+++ Client/OurMemories/app/src/main/kotlin/com/kotlin/ourmemories/view/PlaceListFragment.kt
@@ -16,5 +16,7 @@ class PlaceListFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? =
             inflater?.inflate(R.layout.fragment_place_list, container, false)
 
+//브랜치테스트
 
+    //이것도 
 }
