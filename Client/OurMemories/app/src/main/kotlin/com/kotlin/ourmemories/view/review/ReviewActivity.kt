77002b4b920ccc5a2cb8bf46932f4f092c7f5f3b
minkyu <minkyu9403@gmail.com>
2017-11-10 16:35:10 +0900

    android- review, time capsule View에 Back. 버튼 추가

--- Client/OurMemories/app/src/main/kotlin/com/kotlin/ourmemories/view/review/ReviewActivity.kt
+++ Client/OurMemories/app/src/main/kotlin/com/kotlin/ourmemories/view/review/ReviewActivity.kt
@@ -1,13 +1,23 @@
 package com.kotlin.ourmemories.view.review
 
+import android.graphics.Typeface
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.kotlin.ourmemories.R
+import com.kotlin.ourmemories.view.MainActivity
+import kotlinx.android.synthetic.main.activity_review.*
 
 class ReviewActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_review)
+
+        val canaroExtraBold = Typeface.createFromAsset(this.assets, MainActivity.CANARO_EXTRA_BOLD_PATH)
+        reviewTitleText.typeface = canaroExtraBold
+
+        reviewBack.setOnClickListener {
+            finish()
+        }
     }
 }
--- Client/OurMemories/app/src/main/kotlin/com/kotlin/ourmemories/view/timecapsule/TimeCapsuleActivity.kt
+++ Client/OurMemories/app/src/main/kotlin/com/kotlin/ourmemories/view/timecapsule/TimeCapsuleActivity.kt
@@ -1,13 +1,23 @@
 package com.kotlin.ourmemories.view.timecapsule
 
+import android.graphics.Typeface
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.kotlin.ourmemories.R
+import com.kotlin.ourmemories.view.MainActivity
+import kotlinx.android.synthetic.main.activity_timecapsule.*
 
 class TimeCapsuleActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_timecapsule)
+
+        val canaroExtraBold = Typeface.createFromAsset(this.assets, MainActivity.CANARO_EXTRA_BOLD_PATH)
+        timeCapsuleTitleText.typeface = canaroExtraBold
+
+        timeCapsuleBack.setOnClickListener {
+            finish()
+        }
     }
 }
