246598771b5d4cc566d4f51f0dfd39d5f4859e04
YongSung HEO <yongsungheo@YongSungui-MacBook-Pro.local>
2017-12-03 09:50:40 +0900

    Server - ObjectId to string5

--- Server/project/controllers/printer.py
+++ Server/project/controllers/printer.py
@@ -191,8 +191,7 @@ def multyData():
     info = images.find({"userId" : userId})
     for doc in info:
         if doc['memoryTitle'] == memoryTitle:
-            mId = doc['_id']
-            bson.ObjectId(mId)
+            mId = doc['_id'].str
             # str(mId)
             print(mId)
             break
