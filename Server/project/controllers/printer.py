562a63e3c783670af1b54f9c186f23942bf6a17b
YongSung HEO <yongsungheo@YongSungui-MacBook-Pro.local>
2017-12-05 17:13:59 +0900

    Server - 메모리 데이터베이스에 있는 정보 가져오기

--- Server/project/controllers/printer.py
+++ Server/project/controllers/printer.py
@@ -26,6 +26,7 @@ from project import *
 db = connection.OurMemories # DB 선택
 user = db.users # collection 선택
 images = db.image # image collection 선택
+url = db.imageURL
 
 @app.route('/')
 def index():
@@ -99,11 +100,23 @@ def profile():
     if is_user != None:
         user_object = dict(zip(('userId', 'userName', 'userEmail', 'userProfileImageUrl', 'authLogin'),(is_user['id'],is_user['userName'],is_user['email'],is_user['profile'],"1")))
         sendToAndroid = dict(zip(('isSuccess', 'userProfileResult'), ("true", user_object)))
+
+        # image collection에 있는 id값을 가져온다.
+        memoryArray = [] # 메모리 배열
+        memoryId = images.find_one({"userId" : user_id})
+        if memoryId != None:
+            memories = images.find({"userId" : memoryId})
+            for docs in memories:
+                # memory 데이터를 JSON으로
+                memory_object = dict(zip(('_id', 'memoryTitle', 'memoryFromDate', 'memoryToDate', 'memoryLatitude', 'memoryLongitude', 'memoryNation', 'memoryClassification'),(docs['userId'], docs['memoryTitle'], docs['memoryFromDate'], docs['memoryToDate'], docs['memoryLatitude'], docs['memoryLongitude'], docs['memoryNation'], docs['memoryClassification'] )))
+                memoryArray.append(memory_object)
+                lastSendToAndroid = dict(zip(('isSuccess', 'userLoginResult', 'userLoginMemoryResult'), ('true', user_object, memoryArray) ))
+
     else :
         user_object = dict(zip(('userId', 'userName', 'userEmail', 'userProfileImageUrl', 'authLogin'),(0,0,0,0,"0")))
         sendToAndroid = dict(zip(('isSuccess', 'userProfileResult'), ("false", user_object)))
 
-    return jsonify(sendToAndroid)
+    return jsonify(lastSendToAndroid)
 
 
 # 웹에서 페이스북 로그인을 위한
@@ -118,63 +131,65 @@ def profile():
 #     return facebook.authorize(callback=callback)
 
 
-@app.route('/login/authorized')
-@facebook.authorized_handler
-def facebook_authorized(resp):
-    if resp is None:
-        return 'Access denied: reason=%s error=%s' % (
-            request.args['error_reason'],
-            request.args['error_description']
-        )
-    if isinstance(resp, OAuthException):
-        return 'Access denied: %s' % resp.message
+# 웹에서 페이스북 정보 받는 부분
+# @app.route('/login/authorized')
+# @facebook.authorized_handler
+# def facebook_authorized(resp):
+#     if resp is None:
+#         return 'Access denied: reason=%s error=%s' % (
+#             request.args['error_reason'],
+#             request.args['error_description']
+#         )
+#     if isinstance(resp, OAuthException):
+#         return 'Access denied: %s' % resp.message
+#
+#     session['oauth_token'] = (resp['access_token'], '')
+#     me = facebook.get('/me?fields=id,name,email,picture')
+#     # print(me.data)
+#
+#     # Json 파싱을 통해 값을 가져온다
+#     # 키 값으로 가져온다
+#     user_id = (me.data['id'])
+#     name = (me.data['name'])
+#     email = (me.data['email'])
+#     picture = (me.data['picture']['data']['url'])
+#
+#     # user Id가 DB에 있는지 확인
+#     is_user = user.find_one({"id" : user_id})
+#     if is_user == None:
+#         # isSuccess
+#         isSuccess = 'true/insert'
+#         # transform user data to json
+#         user_object = dict(zip(('userId', 'userName', 'userEmail', 'userProfileImageUrl'),(user_id,name,email,picture)))
+#         # isSuccess와 userLoginResult를 Json으로
+#         sendToAndroid = dict(zip(('isSuccess', 'userLoginResult'), (isSuccess, user_object)))
+#         user.insert({'id' : user_id, 'userName' : name, 'email' : email, 'profile' : picture, 'accessToken' : resp['access_token']})
+#     elif is_user != None:
+#         # isSuccess
+#         isSuccess = 'true/update'
+#         # transform user data to json
+#         user_object = dict(zip(('userId', 'userName', 'userEmail', 'userProfileImageUrl'),(user_id,name,email,picture)))
+#         # isSuccess와 userLoginResult를 Json으로
+#         sendToAndroid = dict(zip(('isSuccess', 'userLoginResult'), (isSuccess, user_object)))
+#         user.update({'id' : user_id}, {'id' : user_id, 'userName' : name, 'email' : email, 'profile' : picture, 'accessToken' : resp['access_token']})
+#     else:
+#         # isSuccess
+#         isSuccess = 'false'
+#         # transform user data to json
+#         user_object = dict(zip(('userId', 'userName', 'userEmail', 'userProfileImageUrl'),(0,0,0,0)))
+#         # isSuccess와 userLoginResult를 Json으로
+#         sendToAndroid = dict(zip(('isSuccess', 'userLoginResult'), (isSuccess, user_object)))
+#     # for doc in is_user:
+#     #     print(doc['id'])
+#
+#     # print(sendToAndroid)
+#     return jsonify(sendToAndroid)
+#
+#     # return 'Logged in as id=%s name=%s email=%s picture=%s redirect=%s' % \
+#     #     (me.data['id'], me.data['name'], me.data['email'], me.data['picture'], request.args.get('next'))
 
-    session['oauth_token'] = (resp['access_token'], '')
-    me = facebook.get('/me?fields=id,name,email,picture')
-    # print(me.data)
 
-    # Json 파싱을 통해 값을 가져온다
-    # 키 값으로 가져온다
-    user_id = (me.data['id'])
-    name = (me.data['name'])
-    email = (me.data['email'])
-    picture = (me.data['picture']['data']['url'])
-
-    # user Id가 DB에 있는지 확인
-    is_user = user.find_one({"id" : user_id})
-    if is_user == None:
-        # isSuccess
-        isSuccess = 'true/insert'
-        # transform user data to json
-        user_object = dict(zip(('userId', 'userName', 'userEmail', 'userProfileImageUrl'),(user_id,name,email,picture)))
-        # isSuccess와 userLoginResult를 Json으로
-        sendToAndroid = dict(zip(('isSuccess', 'userLoginResult'), (isSuccess, user_object)))
-        user.insert({'id' : user_id, 'userName' : name, 'email' : email, 'profile' : picture, 'accessToken' : resp['access_token']})
-    elif is_user != None:
-        # isSuccess
-        isSuccess = 'true/update'
-        # transform user data to json
-        user_object = dict(zip(('userId', 'userName', 'userEmail', 'userProfileImageUrl'),(user_id,name,email,picture)))
-        # isSuccess와 userLoginResult를 Json으로
-        sendToAndroid = dict(zip(('isSuccess', 'userLoginResult'), (isSuccess, user_object)))
-        user.update({'id' : user_id}, {'id' : user_id, 'userName' : name, 'email' : email, 'profile' : picture, 'accessToken' : resp['access_token']})
-    else:
-        # isSuccess
-        isSuccess = 'false'
-        # transform user data to json
-        user_object = dict(zip(('userId', 'userName', 'userEmail', 'userProfileImageUrl'),(0,0,0,0)))
-        # isSuccess와 userLoginResult를 Json으로
-        sendToAndroid = dict(zip(('isSuccess', 'userLoginResult'), (isSuccess, user_object)))
-    # for doc in is_user:
-    #     print(doc['id'])
-
-    # print(sendToAndroid)
-    return jsonify(sendToAndroid)
-
-    # return 'Logged in as id=%s name=%s email=%s picture=%s redirect=%s' % \
-    #     (me.data['id'], me.data['name'], me.data['email'], me.data['picture'], request.args.get('next'))
-
-
+# 웹에서 페이스북 토큰 받는 부분
 @facebook.tokengetter
 def get_facebook_oauth_token():
     return session.get('oauth_token')
@@ -204,12 +219,47 @@ def multyData():
     for doc in info:
         if doc['memoryTitle'] == memoryTitle:
             mId = str(doc['_id'])
-            print(mId)
+            # print(mId)
             break
+
+    image = request.files['uploadFile'] # android에서 보낸 사진 받기
+    print(image)
+
+    imageName = (secure_filename(image.filename)) # 사진 이름만 변수에 저장
+    # print(imageName)
+
+    image.save(os.path.join(app.config['UPLOAD_FOLDER'], imageName)) # 폴더에 이미지 저장
+
+    redirect(url_for('uploaded_file', filename=imageName)) # 웹페이지에 사진을 띄위기 위해
+
+    path = SENDING_IMAGE_PATH + 'uploads/' + imageName # android에 보낼 image URL
+    print(path)
+
+    images.insert({'imageURL' : path})
 
     sendToAndroid = dict(zip(('isSuccess', 'id'), ("true", mId)))
 
     return jsonify(sendToAndroid)
+
+
+@app.route('/memory/view', methods=['GET', 'POST'])
+def memoryView():
+    userId = request.form['id']
+
+    # image collection에 있는 id값을 가져온다.
+    memoryArray = [] # 메모리 배열
+    memoryId = images.find_one({"userId" : user_id})
+    if memoryId != None:
+        memories = images.find({"userId" : memoryId})
+        for docs in memories:
+            media = docs['imageURL']
+            text = docs['text']
+            break;
+
+    memoryItemResult = dict(zip( ('mediaMemory', 'textMemory'), (midia,text) ))
+    memoryItem = dict(zip( ('isSuccess', 'memoryItemResult'), ("true", memoryItemResult) ))
+
+    return jsonify(memoryItem)
 
 
 # 사진 저장(웹페이지에 사진 띄움)
@@ -233,6 +283,6 @@ def multyData():
 
 
 # 아마존 컴퓨터 주소/uploads/filename로 디렉토리에 저장된 파일 꺼내옴
-# @app.route('/uploads/<filename>')
-# def uploaded_file(filename):
-#     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
